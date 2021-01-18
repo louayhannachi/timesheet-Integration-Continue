46f2963d3e694d903234f72c4340a697dac14fcb
lhannachi <lhannachi@vermeg.com>
2021-01-18 18:30:29 +0100

    return old code because it used in the rest controllers

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -1,24 +1,81 @@
 package tn.esprit.spring.services;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.repository.EntrepriseRepository;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class EntrepriseServiceImpl implements IEntrepriseService {
 
 	@Autowired
     EntrepriseRepository entrepriseRepoistory;
+	@Autowired
+	DepartementRepository deptRepoistory;
 	
 	private static final Logger l = LogManager.getLogger(EntrepriseServiceImplTest.class);
+
 	
+	public int ajouterEntreprise(Entreprise entreprise) {
+		entrepriseRepoistory.save(entreprise);
+		return entreprise.getId();
+	}
+
+	public int ajouterDepartement(Departement dep) {
+		deptRepoistory.save(dep);
+		return dep.getId();
+	}
+	
+	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
+		//Le bout Master de cette relation N:1 est departement  
+				//donc il faut rajouter l'entreprise a departement 
+				// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
+				//Rappel : la classe qui contient mappedBy represente le bout Slave
+				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
+				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+				Departement depManagedEntity = deptRepoistory.findById(depId).get();
+				
+				depManagedEntity.setEntreprise(entrepriseManagedEntity);
+				deptRepoistory.save(depManagedEntity);
+		
+	}
+	
+	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
+		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+		List<String> depNames = new ArrayList<>();
+		for(Departement dep : entrepriseManagedEntity.getDepartements()){
+			depNames.add(dep.getName());
+		}
+		
+		return depNames;
+	}
+
+	@Transactional
+	public void deleteEntrepriseById(int entrepriseId) {
+		entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());	
+	}
+
+	@Transactional
+	public void deleteDepartementById(int depId) {
+		deptRepoistory.delete(deptRepoistory.findById(depId).get());	
+	}
+
+
+	public Entreprise getEntrepriseById(int entrepriseId) {
+		return entrepriseRepoistory.findById(entrepriseId).get();	
+	}
+	
+	// home work 
 	@Override
 	public List<Entreprise> retrieveAllEntreprise() {
 		l.info("In  retrieveAllEntreprise : "); 
@@ -58,4 +115,6 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 		return e; 
 	}
 	
+	
+
 }
